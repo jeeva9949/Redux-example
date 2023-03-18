53ed8fb9d307f611a2a35cb8e0d8c9e285ae0253
jeeva9949 <goddumarrijeevananda@gmail.com>
2023-03-18 22:51:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./action/index";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const mystate = useSelector((state) => state.ChangeNumber);
+  console.log(mystate);
+  const dispatch = useDispatch();
+  console.log(dispatch)
+  return ( 
+    <>
+      <Header>Redux in react js </Header>
+      <Counter>
+        <button onClick={() => dispatch(incNumber())}>+</button>
+        <p>{ mystate}</p>
+        <button onClick={() => dispatch(decNumber())}>-</button>
+      </Counter>
+    </>
   );
 }
+
+
+
+const Header = styled.h1`
+  display: flex;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+  color: red;
+  opacity: 0.2;
+`;
+
+const Counter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  button {
+    font-size: 20px;
+    margin: 10px;
+  }
+`;
 
 export default App;
---
+++ src/reducer/index.js
@@ -0,0 +1,9 @@
+
+import ChangeNumber from "./count";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    ChangeNumber,
+})
+export default rootReducer
